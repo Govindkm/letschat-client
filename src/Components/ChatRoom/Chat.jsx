8edd92d5b9cd886dac9542e15ddbe0b44a4fd0e5
Govind Mishra <govindkm.sv@gmail.com>
2022-03-08 23:48:42 +0530

    Update Fix

--- src/Components/ChatRoom/Chat.jsx
+++ src/Components/ChatRoom/Chat.jsx
@@ -1,7 +1,14 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 import SocketContext from "../../Context/SocketContext/SocketContext";
 function Chat() {
   const { Socket, setSocket, Me } = useContext(SocketContext);
+  const navigate = useNavigate();
+  useEffect(() => {
+    if (!Me) {
+      navigate("/");
+    }
+  }, []);
   return <div>{Me}</div>;
 }
 
